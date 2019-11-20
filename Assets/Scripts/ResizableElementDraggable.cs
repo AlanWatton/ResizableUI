81f5d4cf56e44738289bf174dd5d9ed8786ca3b5
iambase <30421546+iambase@users.noreply.github.com>
2019-11-20 13:13:21 +0000

    Adding Demo Project

---
+++ Assets/Scripts/ResizableElementDraggable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class ResizableElementDraggable : MonoBehaviour, IDragHandler
+{
+
+#pragma warning disable 0649
+    [SerializeField] private ResizableElement _resizableElement;
+    [SerializeField] private ResizableElementCorners _corner;
+#pragma warning restore 0649
+
+    private Vector2 _lastKnownPosition;
+
+    private void Awake()
+    {
+        _lastKnownPosition = transform.position;
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        _resizableElement.UpdateSize(_corner, _lastKnownPosition - eventData.position);
+        _lastKnownPosition = eventData.position;
+    }
+}
